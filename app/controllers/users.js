00fac12019278cc8efc7e3cf2c80bdbd84dabd07
Alex Ferreira <alex@dsol.com.br>
2013-06-01 13:14:41 -0300

    Adicionando controller para usuarios com metodo signup, create signup: exibe o formulário pra cadastro de usuário create: cria o usuário, após usuário criado com sucesso é efetuado login e redirecionado para home

---
+++ app/controllers/users.js
@@ -0,0 +1,51 @@
+
+/**
+ * Module dependencies.
+ */
+
+var mongoose = require('mongoose')
+  , User = mongoose.model('User')
+
+/**
+ * Show sign up form
+ */
+
+exports.signup = function (req, res) {
+  res.render('users/signup', {
+    title: 'Sign up',
+    user: new User()
+  })
+}
+
+/**
+ * Create user
+ */
+
+exports.create = function (req, res) {
+  var user = new User(req.body)
+  user.provider = 'local'
+  user.save(function (err) {
+    if (err) {
+      return res.render('users/signup', { errors: err.errors, user: user })
+    }
+    req.logIn(user, function(err) {
+      if (err) return next(err)
+      return res.redirect('/')
+    })
+  })
+}
+
+/**
+ * Find user by id
+ */
+
+exports.user = function (req, res, next, id) {
+  User
+    .findOne({ _id : id })
+    .exec(function (err, user) {
+      if (err) return next(err)
+      if (!user) return next(new Error('Failed to load User ' + id))
+      req.profile = user
+      next()
+    })
+}
