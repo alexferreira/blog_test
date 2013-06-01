29b99828ab22015b869428c48f0974b63224bad5
Alex Ferreira <alex@dsol.com.br>
2013-06-01 13:09:11 -0300

    Adicionando rotas para cadastro de usuários

--- config/routes.js
+++ config/routes.js
@@ -6,7 +6,11 @@
 var async = require('async');
 
 module.exports = function (app, passport, auth) {
-
+  // user routes
+  var users = require('../app/controllers/users')
+  app.get('/signup', users.signup)
+  app.post('/users', users.create)
+  
   var home = require('../app/controllers/home')
   app.get('/', home.index)
 
