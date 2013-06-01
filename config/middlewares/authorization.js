aaa1e2e61d0fa1f182b9c98840532950d043ffdc
Alex Ferreira <alex@dsol.com.br>
2013-06-01 12:32:05 -0300

    Adicionando middlewares requiresLogin

---
+++ config/middlewares/authorization.js
@@ -0,0 +1,12 @@
+
+/*
+ *  Generic require login routing middleware
+ */
+
+exports.requiresLogin = function (req, res, next) {
+  if (!req.isAuthenticated()) {
+    return res.redirect('/login')
+  }
+  next()
+};
+
