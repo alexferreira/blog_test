0c853da0f9bc6d5ba63b67b1edf5cfb95d3bac1a
Alex Ferreira <alex@dsol.com.br>
2013-06-02 05:39:42 -0300

    Adicionando middleware hasAuthorization para posts, será usado nas toras de edição, atualização ou exclusão de postagem. Sendo assim um post criado por um usuário não poderá ser alterado por outro.

--- config/middlewares/authorization.js
+++ config/middlewares/authorization.js
@@ -7,6 +7,18 @@ exports.requiresLogin = function (req, res, next) {
   if (!req.isAuthenticated()) {
     return res.redirect('/login')
   }
-  next()
+  next();
 };
 
+/*
+ *  Post authorizations routing middleware
+ */
+
+exports.post = {
+  hasAuthorization : function (req, res, next) {
+    if (req.post.user.id != req.user.id) {
+      return res.redirect('/posts/'+req.post.id)
+    }
+    next();
+  }
+}
