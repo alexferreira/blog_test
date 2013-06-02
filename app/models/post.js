87169731eba01dd0a42585ae4e8b1085c1cea41c
Alex Ferreira <alex@dsol.com.br>
2013-06-02 06:03:41 -0300

    Adicionando funcões getTags e setTags para tags

--- app/models/post.js
+++ app/models/post.js
@@ -7,6 +7,22 @@ var mongoose = require('mongoose')
   , Schema = mongoose.Schema
 
 /**
+ * Getters
+ */
+
+var getTags = function (tags) {
+  return tags.join(',')
+}
+
+/**
+ * Setters
+ */
+
+var setTags = function (tags) {
+  return tags.split(',')
+}
+
+/**
  * Post Schema
  */
 
