0c69fcdde4ce7956b518b7506b440189bcd97326
Alex Ferreira <alex@dsol.com.br>
2013-06-02 05:42:18 -0300

    Adicionando model post

---
+++ app/models/post.js
@@ -0,0 +1,26 @@
+
+/**
+ * Module dependencies.
+ */
+
+var mongoose = require('mongoose')
+  , Schema = mongoose.Schema
+
+/**
+ * Post Schema
+ */
+
+var PostSchema = new Schema({
+  title: {type : String, default : '', trim : true},
+  body: {type : String, default : '', trim : true},
+  user: {type : Schema.ObjectId, ref : 'User'},
+  comments: [{
+    body: { type : String, default : '' },
+    user: { type : Schema.ObjectId, ref : 'User' },
+    createdAt: { type : Date, default : Date.now }
+  }],
+  tags: {type: [], get: getTags, set: setTags},
+  createdAt  : {type : Date, default : Date.now}
+})
+
+mongoose.model('Post', PostSchema)
