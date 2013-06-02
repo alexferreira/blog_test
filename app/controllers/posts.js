2c6a2cddb3eb840c2aab28b945cfbb546aa24022
Alex Ferreira <alex@dsol.com.br>
2013-06-02 06:11:03 -0300

    Adicionando metodo create

--- app/controllers/posts.js
+++ app/controllers/posts.js
@@ -14,8 +14,29 @@ var mongoose = require('mongoose')
 
 exports.new = function(req, res){
   res.render('posts/new', {
-    title: 'New Post',
+    title: 'Novo Post',
     post: new Post({}),
     tags: req.tags
+  })
+}
+
+/**
+ * Create an post
+ */
+
+exports.create = function (req, res) {
+  var post = new Post(req.body)
+  post.user = req.user
+
+  post.save(function (err) {
+    if (err) {
+      res.render('posts/new', {
+        title: 'Novo Post',
+        post: post,
+        errors: err.errors
+      })
+    } else {
+      res.redirect('/posts/'+post._id)
+    }
   })
 }
