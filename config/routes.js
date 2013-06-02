7954710f13323ac22cf550650c7ace2ea1ecc9a2
Alex Ferreira <alex@dsol.com.br>
2013-06-02 09:14:11 -0300

    Adicionando rotas para postagens e filtro de tags.

--- config/routes.js
+++ config/routes.js
@@ -3,11 +3,16 @@
  * Module dependencies.
  */
 
-var async = require('async');
+var async = require('async')
+  , users = require('../app/controllers/users')
+  , tags = require('../app/controllers/tags')
+  , posts = require('../app/controllers/posts')
+  , comments = require('../app/controllers/comments');
 
 module.exports = function (app, passport, auth) {
+  app.get('*', tags.list)
+
   // user routes
-  var users = require('../app/controllers/users')
   app.get('/signup', users.signup)
   app.post('/users', users.create)
   app.get('/logout', users.logout)
@@ -15,7 +20,24 @@ module.exports = function (app, passport, auth) {
   app.post('/users/session', passport.authenticate('local', {failureRedirect: '/login', failureFlash: 'Invalid email or password.'}), users.session)
   app.get('/users/:userId', users.show)
   
-  var home = require('../app/controllers/home')
-  app.get('/', home.index)
+  app.param('userId', users.user)
 
+  // post routes
+  app.get('/posts', posts.index)
+  app.get('/posts/new', auth.requiresLogin, posts.new)
+  app.post('/posts', auth.requiresLogin, posts.create)
+  app.get('/posts/:id', posts.show)
+  app.get('/posts/:id/edit', auth.requiresLogin, auth.post.hasAuthorization, posts.edit)
+  app.put('/posts/:id', auth.requiresLogin, auth.post.hasAuthorization, posts.update)
+  app.del('/posts/:id', auth.requiresLogin, auth.post.hasAuthorization, posts.destroy)
+
+  app.param('id', posts.post)
+  
+  app.get('/', posts.index)
+
+  // comment routes
+  app.post('/posts/:id/comments', comments.create)
+
+  // tag routes
+  app.get('/tags/:tag', tags.index)
 }
