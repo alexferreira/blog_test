7990c1c5c2a297ccf004db8164dac472171f165a
Alex Ferreira <alex@dsol.com.br>
2013-06-02 09:13:11 -0300

    Adicionando mensagem de erro quando campos de comentário não forem preenchidos

--- app/controllers/comments.js
+++ app/controllers/comments.js
@@ -12,7 +12,11 @@ var mongoose = require('mongoose')
 exports.create = function (req, res) {
   var post = req.post;
 
-  if (!req.body.email || !req.body.body) return res.redirect('/posts/'+ post.id)
+  if (!req.body.email || !req.body.body) {
+    if(!req.body.email) req.flash('error', 'O Email deve ser preenchido')
+    if(!req.body.body) req.flash('error', 'O Comentário deve ser preenchido')
+    return res.redirect('/posts/'+ post.id)
+  }
 
   post.addComment(req.body, function (err) {
     if (err) return res.render('500')
